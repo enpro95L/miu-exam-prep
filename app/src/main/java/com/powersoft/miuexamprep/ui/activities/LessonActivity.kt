a731fa906ebde53407880380792076c5c9daee83
Derrick Korku <derrickkorku1@gmail.com>
2023-04-24 14:14:57 -0500

    Update

--- app/src/main/java/com/powersoft/miuexamprep/ui/activities/LessonActivity.kt
+++ app/src/main/java/com/powersoft/miuexamprep/ui/activities/LessonActivity.kt
@@ -3,28 +3,40 @@ package com.powersoft.miuexamprep.ui.activities
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.powersoft.miuexamprep.db.AppDatabase
 import com.powersoft.miuexamprep.databinding.ActivityLessonBinding
 import com.powersoft.miuexamprep.model.Course
+import com.powersoft.miuexamprep.view_models.CourseViewModel
+import com.powersoft.miuexamprep.view_models.LessonViewModel
 import kotlinx.coroutines.launch
 
 class LessonActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLessonBinding
+    private lateinit var viewModel: LessonViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLessonBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        )[LessonViewModel::class.java]
+
         val course = intent.getSerializableExtra("course") as Course
+
+        viewModel.setCourseId(course.id)
+
         Toast.makeText(this, "${course.id}", Toast.LENGTH_SHORT).show()
 
         binding.toolbar.tvTitle.text = course.name
 
-//        lifecycleScope.launch {
-//            AppDatabase.invoke(this@LessonActivity).lessonDao().getLessons(10)
-//        }
+        viewModel.courseLessons.observe(this){courseLessons ->
+
+        }
     }
 }
