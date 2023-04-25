1421263c561662585e266870d8b15dc3ee746db8
Derrick Korku <derrickkorku1@gmail.com>
2023-04-25 13:34:19 -0500

    update

--- app/src/main/java/com/powersoft/miuexamprep/db/seeders/MCQSeeder.kt
+++ app/src/main/java/com/powersoft/miuexamprep/db/seeders/MCQSeeder.kt
@@ -1,31 +1,26 @@
 package com.powersoft.miuexamprep.db.seeders
 
-import android.util.Log
 import com.powersoft.miuexamprep.model.MCQ
 
 object MCQSeeder {
     var mcqs = mutableListOf<MCQ>()
-    private fun androidLessons(){
-        val androids = AndroidQuestions.questions
-        for (lesson in androids.keys){
-            var lessonId = lesson
-            var questions = androids.get(lessonId)
-            var answers = AndroidQuestions.anwers.get(lessonId)!!
-
-            for ((index, question) in questions?.keys!!.withIndex()){
-                Log.i("test1", lessonId.toString())
-                Log.i("test2", question.toString())
-
-                mcqs.add(
-                    MCQ(1, 1, lessonId, question, questions.get(question)!!, answers[index])
+    private fun androidLesson2() {
+        for ((index, question) in AndroidQuestions.lesson2.keys.withIndex()) {
+            mcqs.add(
+                MCQ(
+                    index + 1,
+                    1,
+                    2,
+                    question,
+                    AndroidQuestions.lesson2[question]!!,
+                    AndroidQuestions.lesson2Answers[index]
                 )
-            }
+            )
         }
     }
 
-    fun getMCQs(): List<MCQ>{
-        androidLessons()
-        Log.i("test3", mcqs.toString())
+    fun getMCQs(): List<MCQ> {
+        androidLesson2()
         return mcqs
     }
 }
