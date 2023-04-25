9730a234f68600d48ca890be17e3ad6e6c57394e
Derrick Korku <derrickkorku1@gmail.com>
2023-04-25 13:13:38 -0500

    update

--- app/src/main/java/com/powersoft/miuexamprep/db/seeders/MCQSeeder.kt
+++ app/src/main/java/com/powersoft/miuexamprep/db/seeders/MCQSeeder.kt
@@ -1,5 +1,6 @@
 package com.powersoft.miuexamprep.db.seeders
 
+import android.util.Log
 import com.powersoft.miuexamprep.model.MCQ
 
 object MCQSeeder {
@@ -12,6 +13,9 @@ object MCQSeeder {
             var answers = AndroidQuestions.anwers.get(lessonId)!!
 
             for ((index, question) in questions?.keys!!.withIndex()){
+                Log.i("test1", lessonId.toString())
+                Log.i("test2", question.toString())
+
                 mcqs.add(
                     MCQ(1, 1, lessonId, question, questions.get(question)!!, answers[index])
                 )
@@ -21,6 +25,7 @@ object MCQSeeder {
 
     fun getMCQs(): List<MCQ>{
         androidLessons()
+        Log.i("test3", mcqs.toString())
         return mcqs
     }
 }
