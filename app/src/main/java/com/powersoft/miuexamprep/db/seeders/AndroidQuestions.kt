e32ea5e5266bfc28a3820d654cd4bf53325cfbf0
Derrick Korku <derrickkorku1@gmail.com>
2023-04-24 21:27:07 -0500

    update

--- app/src/main/java/com/powersoft/miuexamprep/db/seeders/AndroidQuestions.kt
+++ app/src/main/java/com/powersoft/miuexamprep/db/seeders/AndroidQuestions.kt
@@ -10,7 +10,23 @@ object AndroidQuestions {
         "'for (i in 1..5) print(i)' is valid statement " to listOf("A.True", "B.False"),
         "In Kotlin, variables can be null by default" to listOf("A.True", "B.False"),
         "Allow null-pointer exceptions using the !! operator" to listOf("A.True", "B.False"),
-
+        "In Kotlin, we test for null using the elvis?" to listOf("A.?:", "B.!", "isNUll()"),
+        "In Kotlin, all data types are of the primitive type" to listOf("A. True", "B.False"),
+        "Which of the following is the safe call operator in Kotlin" to listOf("A.?", "B.!!", "C.?:"),
+        "Which of the following will throw an exception if null?" to listOf("A.?", "B.!!", "C.?:")
     )
-    val lesson2Answers: List<String> = listOf("B.Verbose", "A.True", "A.True", "A.True", "B.False", "A.True")
+    val lesson2Answers: List<String> = listOf("B.Verbose", "A.True", "A.True", "A.True", "B.False", "A.True", "A.?:", "B.False", "A.?", "B.!!")
+
+
+    val lesson3: Map<String, List<String>> = mapOf(
+        "Handles user input and creates a window on the screen to display your user interface" to listOf<String>("A.Activity", "B.Resources", "C.Graddle"),
+        "They are additional files that your code uses, such as layout files, images, audio files, themes, colors, and more." to listOf<String>("A.Activity", "B.Resources", "C.Graddle"),
+        "_____________ - declares essential information for your app" to listOf<String>("A.app", "B.AndroidManifest.xml", "C.Graddle"),
+        "________________ - controls how your application builds, tests, and deploys itself." to listOf<String>("A.app", "B.AndroidManifest.xml", "C.Graddle"),
+        "________________ - are they interface building blocks." to listOf<String>("A.View", "B.AndroidManifest.xml", "C.Graddle"),
+        "Use a ______________________ if you have only one child View." to listOf<String>("A.LinearLayout", "B.ConstraintLayout", "C.FrameLayout"),
+        "Use a ______________________ to display views in a row or column." to listOf<String>("A.LinearLayout", "B.ConstraintLayout", "C.FrameLayout"),
+        "Use a ______________________ for more complex layouts." to listOf<String>("A.LinearLayout", "B.ConstraintLayout", "C.FrameLayout")
+    )
+    val lesson3Answers: List<String> = listOf("A.Activity", "B.Resources", "B.AndroidManifest.xml", "C.Graddle", "A.View", "C.FrameLayout", "A.LinearLayout", "B.ConstraintLayout",)
 }
--- app/src/main/java/com/powersoft/miuexamprep/db/seeders/LessonSeeder.kt
+++ app/src/main/java/com/powersoft/miuexamprep/db/seeders/LessonSeeder.kt
@@ -5,19 +5,18 @@ import com.powersoft.miuexamprep.model.Lesson
 object LessonSeeder {
     operator fun invoke(): List<Lesson>{
         return listOf(
-            Lesson(1, "Introduction", 10, 1),
-            Lesson(2, "Kotlin Fundamentals", 10, 1),
-            Lesson(3, "Creating First App", 10, 1),
-            Lesson(4,"Activity Lifecycle", 10, 1),
-            Lesson(5,"Activities Intents", 10, 1),
-            Lesson(6,"User Inputs Controls", 10, 1),
+            Lesson(1, "Kotlin Fundamentals", 10, 1),
+            Lesson(2, "Creating First App", 10, 1),
+            Lesson(3,"Activity Lifecycle", 10, 1),
+            Lesson(4,"Activities Intents", 10, 1),
+            Lesson(5,"User Inputs Controls", 10, 1),
 
-            Lesson(7, "Objects", 10, 2),
-            Lesson(8, "Inheritance", 10, 2),
-            Lesson(9, "Swing", 10, 2),
-            Lesson(10,"Recursions", 10, 2),
-            Lesson(11,"Datastructures", 10, 2),
-            Lesson(12,"LinkedList", 10, 2)
+            Lesson(6, "Objects", 10, 2),
+            Lesson(7, "Inheritance", 10, 2),
+            Lesson(8, "Swing", 10, 2),
+            Lesson(9,"Recursions", 10, 2),
+            Lesson(10,"Datastructures", 10, 2),
+            Lesson(11,"LinkedList", 10, 2)
         )
     }
 }
